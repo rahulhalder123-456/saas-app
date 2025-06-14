7147ba5b37b1f1fc6d265dc1b43f01944f88f1b6
rahulhalder123-456 <rahulhalder123987@gmail.com>
2025-06-14 23:36:22 +0530

    feat: refactor Page component to fetch companions and recent sessions dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,24 @@ import CompanionCard from "@/components/CompanionCard";
 import CompanionList from "@/components/CompanionList";
 import CTA from "@/components/CTA";
 import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({limit:3});
+  const recentSessionsCompanions = (await getRecentSessions()).flat();
     return (
     <main>
       <h1>Popular companions</h1>
         <section className={"home-section"}>
-            <CompanionCard id={'1'} name={'Neura the Brainy Explorer'} topic={'Neural Network of the Brain'} subject={'Science'} duration={45} color={'#E5D0FF'}/>
-            <CompanionCard id={'2'} name={'Countsy the Number Wizard'} topic={'Derivatives & Integrals'} subject={'maths'} duration={30} color={'#FFDA6E'}/>
-            <CompanionCard id={'3'} name={'Verba the Vocabulary Builder'} topic={'English Literature'} subject={'language'} duration={30} color={'#BDE7FF'}/>
+            {companions.map(companion => (
+              <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+            ))}
         </section>
         <section className={"home-section"}>
             <CompanionList
                 title={"Recently completed sessions"}
-                companions={recentSessions}
+                companions={recentSessionsCompanions}
                 classNames={'w-2/3 max-lg:w-full'}
             />
             <CTA/>
